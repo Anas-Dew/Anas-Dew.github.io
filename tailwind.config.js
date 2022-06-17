32841c265fc0d586414c539c28c0fc72d913d7b6
anas-dew <mailtoanas@duck.com>
2022-06-17 16:43:28 +0530

    font changed profile pic animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
       'sans' : ['Fira sans'],
       'mono' : ['monospace'],
       'karla' : ['Karla', 'sans-serif'],
-      'mulish' : ['Mulish','sans-serif']
+      'mulish' : ['Mulish','sans-serif'],
+      'abril' : ['Abril Fatface', 'cursive'],
+      'cinzel' : ['Cinzel', 'serif'],
+      'Fredericka': ['Fredericka the Great', 'cursive'],
+      'darling': ['Ingrid Darling', 'cursive'],
+      'joan': ['Joan', 'serif'],
+      'raggae': ['Reggae One', 'cursive'],
+      'glitch': ['Rubik Glitch', 'cursive'],
+      'tapestry': ['Tapestry', 'cursive'],
+      'baloo 2' : ['Baloo Chettan 2', 'cursive']
     },
     screens:{
       'mobile': "400px"
